ac116a8bed56fcdd02e1d39a50d49b009922df8b
森脇暉 <moriwakihikari@moriwakiakiranoMacBook-Air.local>
2022-06-30 11:17:00 +0900

    firebaseのauth機能追加,gormの追加

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+type User struct {
+	gorm.Model
+	ID       uint
+	Name     string
+	Age      int
+	Birthday time.Time
+}
 
 func main() {
 	r := gin.Default()
@@ -10,4 +31,29 @@ func main() {
 		})
 	})
 	r.Run() // 0.0.0.0:8080 でサーバーを立てます。
+
+	// sqlDB, _ := sql.Open("mysql", "mydb_dsn")
+	// gormdb, _ := gorm.Open(mysql.New(mysql.Config{
+	// 	Conn: sqlDB,
+	// }), &gorm.Config{})
+
+	// user := User{Name: "Jinzhu", Age: 18, Birthday: time.Now()}
+
+	// result := gormdb.Create(&user) // pass pointer of data to Create
+
+	// user.ID             // returns inserted data's primary key
+	// result.Error        // returns error
+	// result.RowsAffected // returns inserted records count
+
+	// dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// db, err := sql.Open("mysql", "user:password@/dbname")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// // See "Important settings" section.
+	// db.SetConnMaxLifetime(time.Minute * 3)
+	// db.SetMaxOpenConns(10)
+	// db.SetMaxIdleConns(10)
 }
